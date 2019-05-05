f5058c181b35adedd1eb72a3b845925cf967a367
unknown <qsc5367899@gmail.com>
2019-05-05 23:32:53 +0800

    first commit

---
+++ CMS.BLL/Services/CustomerService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CMS.DAL;
+using CMS.DAL.Repository;
+using CMS.Domain;
+using CMS.DAL.Interfaces;
+using CMS.Domain.ViewModels;
+using AutoMapper;
+
+namespace CMS.BLL.Services
+{
+    public class CustomerService
+    {
+        private IRepository<Customers> db;
+
+        public CustomerService()
+        {
+            db = new GenericRepository<Customers>();
+        }
+
+        public List<CustomerViewModel> Get()
+        {
+            var DbResult = db.Get().ToList(); ;
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<Customers, CustomerViewModel>();
+            });
+
+            IMapper mapper = config.CreateMapper();
+            return mapper.Map<List<Customers>, List<CustomerViewModel>>(DbResult);
+
+            /*List<CustomerViewModel> model = new List<CustomerViewModel>();
+            foreach(var item in DbResult)
+            {
+                CustomerViewModel _model = new CustomerViewModel();
+                _model.CustomerID = item.CustomerID;
+                _model.ContactName = item.ContactName;
+                model.Add(_model);
+            }
+            return model.AsQueryable();*/
+        }
+    }
+}
---
+++ CMS.Web/API/CustomerController.cs
@@ -0,0 +1,56 @@
+﻿using CMS.BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CMS.Web.API
+{
+    public class CustomerController : ApiController
+    {
+        private CustomerService service;
+
+        private CustomerController()
+        {
+            service = new CustomerService();
+        }
+
+        // GET: api/Empty
+        [HttpGet]
+        public HttpResponseMessage Customers()
+        {
+            try
+            {
+                var datas = service.Get();
+                return Request.CreateResponse(HttpStatusCode.OK, datas);
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message.ToString());
+            }
+        }
+
+        // GET: api/Empty/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Empty
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Empty/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Empty/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
